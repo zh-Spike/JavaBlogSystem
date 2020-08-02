1a9033187f87015c264d39149beff5e5d4a7e10a
ETNJL <etnjl@outlook.com>
2020-08-02 21:38:38 +0800

    logConfiguration

--- src/main/java/net/blog/BlogApplication.java
+++ src/main/java/net/blog/BlogApplication.java
@@ -1,11 +1,14 @@
 package net.blog;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class BlogApplication {
     public static void main(String[] args) {
+        log.info("BlogApplication run...");
         SpringApplication.run(BlogApplication.class, args);
     }
 }
--- src/main/java/net/blog/controller/TestController.java
+++ src/main/java/net/blog/controller/TestController.java
@@ -1,23 +1,26 @@
 package net.blog.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import net.blog.pojo.User;
 import net.blog.response.ResponseResult;
+
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/test")
 public class TestController {
 
    @GetMapping("/Hello-world")
     public ResponseResult helloWorld() {
-       System.out.println("Hello World!");
+       log.info("Hello World!");
        return ResponseResult.SUCCESS().setData("Hello");
     }
 
     @PostMapping("/test-login")
     public ResponseResult testlogin(@RequestBody User user){
-       System.out.println("user name -== > "+user.getUser_name());
-       System.out.println("password -== > "+user.getPassword());
+       log.info("user name -== > "+user.getUser_name());
+       log.info("password -== > "+user.getPassword());
        return ResponseResult.SUCCESS("登陆成功");
     }
 }
